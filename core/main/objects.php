755bf9d9edc25aaa151bf6669bfa33ee3528ddb4
Andrew Zenin <kashaket@yandex.ua>
2019-02-13 14:50:03 +0200

    Optimization

--- core/main/objects.php
+++ core/main/objects.php
@@ -1191,7 +1191,8 @@ function val($str, $value = null){
 
 function __autoload($name)
 {
-	if( substr($name, 0, 2) == 'ev' ) return;
+	if( substr($name, 0, 2) == 'ev' or  substr($name, 0, 7) == 'modifer' ) return;
+
 		if( gui_class_isset($name) )
 			eval("class $name extends TControl{}");
 }
