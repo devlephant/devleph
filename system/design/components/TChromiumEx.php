1883645f0299d1131e4afdbe80d0e0efba914e5a
Andrew Zenin <kashaket@yandex.ua>
2019-05-25 17:00:01 +0300

    Bugfix

--- system/design/components/TChromiumEx.php
+++ system/design/components/TChromiumEx.php
@@ -8,7 +8,7 @@
 'W' =>40,
 'H' =>30,
 'USE_SKIN' =>true,
-
+'REPLACE' => 'THtmlViewer',
 'DLLS' => ['locales', 'swiftshader', 'cef_sandbox.lib', 'cef.pak', 'cef_100_percent.pak', 'cef_200_percent.pak', 'cef_extensions.pak',
 'chrome_elf.dll', 'd3dcompiler_43.dll', 'd3dcompiler_47.dll', 'devtools_resources.pak', 'icudtl.dat',
 'libcef.dll', 'libcef.lib', 'libEGL.dll', 'libGLESv2.dll', 'natives_blob.bin', 'snapshot_blob.bin', 'v8_context_snapshot.bin'],
