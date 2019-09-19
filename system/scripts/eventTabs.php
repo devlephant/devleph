1bcb8e9becaf565290ca6c6b9d371178b8ae5b83
Andrew Zenin <kashaket@yandex.ua>
2019-09-19 08:13:43 +0300

    Revert "[ Ref ] eventTabs"
    This reverts commit 373333d5c052e34a51c7de957828623d0647d653.

--- system/scripts/eventTabs.php
+++ system/scripts/eventTabs.php
@@ -13,24 +13,3 @@ function eventTabs_update(){
 	$eventTabs->text = implode(PHP_EOL, $events);
 	$eventTabs->addPage('+');
 }
-
-function eventTabs_show(){
-		global $myEvents, $_FORMS, $formSelected;
-		eventEngine::setForm();
-		$eventList = c('fmPropsAndEvents->eventList');
-		$eventTabs = c('fmPHPEditor->eventTabs');
-		$eventTabs->popupMenu = c('fmMain->edt_EventTypes->popupMenu');
-		$php_memo = c('fmPHPEditor->memo');
-		
-		$name = $myEvents->selObj instanceof TForm ? '--fmedit' : $myEvents->selObj->name;
-		$event  = $eventList->events[$eventList->itemIndex];
-		$events = eventEngine::listEventsEx($name);
-		$eventTabs->text = implode(PHP_EOL, $events);
-		$eventTabs->addPage('+');
-		$eventTabs->last_index = $eventTabs->TabIndex = $eventList->itemIndex;
-        
-        $php_memo->text = eventEngine::getEvent($name, $event);
-        $ltight = str_replace('{', '', str_ireplace('event ', '', CApi::getStringEventInfo($event, $myEvents->selObj->className) ) );
-        $x_name = $myEvents->selObj->name == 'fmEdit' ? $_FORMS[$formSelected] : $myEvents->selObj->name;
-        c('fmPHPEditor')->text = t('php_script_editor').' -> '.$x_name.'::'.$ltight;
-}
